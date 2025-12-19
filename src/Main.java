730f40a2d3ea4c4eedaf8e8d3a605b0f331f9ad4
Nagini <emresekizsu@gmail.com>
2025-12-19 20:08:12 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -307,6 +307,12 @@ public class Main{
         }
 
         System.out.println("Bilgi: JSON'dan " + jsonOgrencileri.size() + " öğrenci sisteme yüklendi.");
+        mekanlar.add(new Derslik("D-101", "Bina A", 40));
+        try {
+            mekanlar.add(new Laboratuvar("LB-1", "Bina B", 30, 25));
+        } catch (Exception e) {
+            System.out.println("Laboratuvar oluşturulurken hata: " + e.getMessage());
+        }
     }
 
 }
