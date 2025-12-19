5348ebf3aff0929ea7a3b61942dcdc24f864b94b
Nagini <emresekizsu@gmail.com>
2025-12-19 19:36:30 +0300

    AkademikMekan

--- src/AkademikMekan.java
+++ src/AkademikMekan.java
@@ -1,6 +1,4 @@
-// Hiyerarşi 2'nin Başı
-
-    public abstract class AkademikMekan implements IRezervasyon {
+public abstract class AkademikMekan implements IRezervasyon {
     // Değişkenler private (Encapsulation) ve Wrapper sınıf (Integer)
     private String isim;
     private String konum;
@@ -11,16 +9,34 @@
         this.konum = konum;
         this.kapasite = kapasite;
     }
-
     // Soyut Metod: Her alt sınıf bunu kendine göre DOLDURMAK ZORUNDA
     public abstract void ozellikleriListele();
+
+    // Yeni metot: verilen kisiSayisi ile mekanda ders işlenip işlenemeyeceğini sorgular
+    // Alt sınıflar bunu override ederek kendi kararlarını verebilir. Burada bir varsayılan
+    // (default) uygulama sağlanıyor; böylece tüm alt sınıfları değiştirmeye gerek kalmaz.
+    public boolean kapasiteSorgula(int kisiSayisi) {
+        boolean uygun = kisiSayisi <= this.kapasite;
+        if (uygun) {
+            System.out.println(isim + " için " + kisiSayisi + " kişilik etkinlik/ders yapılabilir (varsayılan kontrol).");
+        } else {
+            System.out.println(isim + " için " + kisiSayisi + " kişilik etkinlik/ders yapılamaz. Kapasite: " + kapasite);
+        }
+        return uygun;
+    }
+
+    // Yeni metot: Ders ataması yapmak için alt sınıflar override edebilir.
+    // Eğer alt sınıf override etmezse bu varsayılan davranış çalışır.
+    public boolean dersAtama(String dersAdi, String onaylayanAdi) {
+        System.out.println("[AkademikMekan] " + getIsim() + " bu mekan türü ders atamayı desteklemiyor veya özel davranışı yok.");
+        return false;
+    }
 
     // Getter ve Setter Metodları (Erişim için)
     public String getIsim() { return isim; }
     public Integer getKapasite() { return kapasite; }
     // Interface'den gelen metodu burada genel bir şekilde ezebiliriz
     // veya alt sınıflara bırakabiliriz. Burada örnek gövde yazalım:
-
     @Override
     public void rezervasyonYap(String zaman, int kisiSayisi) throws KapasiteHatasiException {
         if (kisiSayisi > this.kapasite) {
